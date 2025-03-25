091b5828a843c46cfcad25e1d2941f43d71c358b
rashokatwal <rashokatwalHK@gmail.com>
2025-03-25 22:34:20 +0530

    Enhance Sell page layout with form for product submission

--- frontend/src/pages/Sell.jsx
+++ frontend/src/pages/Sell.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Sell = () => {
   return (
-    <div>
+    <div className='p-6'>
+      <h1 className='mb-2 text-xl font-bold'>Sell Your Product</h1>
+      <p className='mb-4 text-gray-500'>Start selling your products quickly and easily.</p>
       
+      <form className='flex flex-col gap-3'>
+        <input type='text' placeholder='Product Name' className='p-2 border rounded'/>
+        <input type='number' placeholder='Price' className='p-2 border rounded'/>
+        <textarea placeholder='Description' className='p-2 border rounded'></textarea>
+        <button className='p-2 text-white bg-blue-500 rounded hover:bg-blue-600'>Submit</button>
+      </form>
     </div>
   )
 }
