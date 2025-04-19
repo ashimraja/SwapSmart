1a4c1607f5e39dc09ea1c21d088713868f251d4c
sagar ghimire <sagarghimire878@gmail.com>
2025-04-20 00:06:40 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Navbar = ({setToken}) => {
   return (
-    <div className='flex items-center py-2 px-[4%] justify-between'>
+    <div  className='flex items-center py-2 px-[4%] justify-between'>
       <h1>SwapSmart</h1>
       <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
